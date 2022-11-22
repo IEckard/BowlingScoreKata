08d8e103a301e745e00c099a5ad02219a0d3afa8
Irena Eckard <irena.eckard@accenture.com>
2022-11-22 13:12:11 -0500

    Improved Game class & roll()

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,19 +1,36 @@
 public class Game {
 
-    int[][] scoreboard = new int [2][11];
-    int currentRoll = 0;
-    int currentFrame = 0;
+    int[][] scoreboard;
+    int currentRoll;
+    int currentFrame;
 
-    public void roll(int pins){
-        scoreboard[currentRoll][currentFrame] = pins;
-        currentRoll++;
-        if (pins == 10) { //skips next roll if a strike
-            currentRoll++;
-        }
-        if (currentRoll == 2) { //moves to next frame, resets rolls
-            currentFrame++;
-            currentRoll = 0;
-        }
 
+    public Game() {
+        this.currentRoll = 0;
+        this.currentFrame = 0;
+        this.scoreboard = new int[2][11];
+    }
+
+    //TODO: create tests for roll()
+    public void roll(int pins) {
+        int currentFrameScore = this.scoreboard[0][currentFrame] + this.scoreboard[1][currentFrame];
+
+        if (!(pins <= 10 & pins >= 0)) {
+            System.out.println("Please enter a number from 0 to 10.");
+        } else if (!(currentFrameScore + pins <= 10)) {
+            System.out.println("Please enter a number from 0 to " + (10 - currentFrameScore));
+        } else {
+            this.scoreboard[this.currentRoll][this.currentFrame] = pins; //updates scoreboard matrix
+            this.currentRoll++;
+
+            if (pins == 10) { //skips next roll if a strike
+                this.currentRoll++;
+            }
+            if (this.currentRoll >= 2) { //moves to next frame, resets roll counter
+                this.currentFrame++;
+                this.currentRoll = 0;
+            }
+        }
     }
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
-package main.java;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Starting a new Bowling Game!");
+        Game game = new Game();
+        Scanner newRoll = new Scanner(System.in);
+
+    //TODO: write score intake (use while statement?) that only accepts
+        // enough rolls to fill the scoreboard. Write score() method in
+        // Game class, return and print result.
+
+
+
     }
 }
